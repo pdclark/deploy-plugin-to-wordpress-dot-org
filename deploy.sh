4139aa0d48f82d6f5f9e28ac4003d74e7966a4da
Paul Clark <pdclark@brainstormmedia.com>
2013-06-23 22:46:21 -0800

    Auto-detect plugin slug and directory; Move settings to top.

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,21 @@
 #! /bin/bash
+# Source: https://raw.github.com/thenbrent/multisite-user-management/master/deploy.sh
+# http://thereforei.am/2011/04/21/git-to-svn-automated-wordpress-plugin-deployment/
+# A modification of Dean Clatworthy's deploy script as found here: https://github.com/deanc/wordpress-plugin-git-svn
+# The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
 
-# Install this file at the top level of your Git repository
-
-# User config
-PLUGINSLUG="example-plugin"   # Slug on wordpress.org (from the plugin URL)
-MAINFILE="example-plugin.php" # Name of main php file in the WordPress plugin
-SVNUSER="username"            # SVN username on wordpress.org
+# main config
+PLUGINSLUG=${PWD##*/} # returns basename of current directory
+CURRENTDIR=`pwd`
+MAINFILE="styles.php" # this should be the name of your main php file in the wordpress plugin
 
 # git config
-CURRENTDIR=`pwd`
-GITPATH="$CURRENTDIR/" 
+GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNUSER="pdclark" # your svn username
 
 
 # Let's begin...
@@ -25,26 +27,26 @@ echo ".........................................."
 echo 
 
 # Check version in readme.txt is the same as plugin file
-NEWVERSION1=`grep "^Stable tag" $GITPATH/readme.txt | awk -F' ' '{print $3}'`
+NEWVERSION1=`grep "^Stable tag" $GITPATH/readme.txt | awk -F' ' '{print $3}' | sed 's/[[:space:]]//g'`
 echo "readme version: $NEWVERSION1"
-NEWVERSION2=`grep "^Version" $GITPATH/$MAINFILE | awk -F' ' '{print $2}'`
+NEWVERSION2=`grep "^Version" $GITPATH/$MAINFILE | awk -F' ' '{print $2}' | sed 's/[[:space:]]//g'`
 echo "$MAINFILE version: $NEWVERSION2"
 
-if [ "$NEWVERSION1" != "$NEWVERSION2" && "$NEWVERSION1" != "trunk" ]; then echo "Versions don't match. Exiting...."; exit 1; fi
+if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Versions don't match. Exiting...."; exit 1; fi
 
 echo "Versions match in readme.txt and PHP file. Let's proceed..."
 
 cd $GITPATH
 echo -e "Enter a commit message for this new version: \c"
 read COMMITMSG
-git commit -am "$COMMITMSG"
+# git commit -am "$COMMITMSG"
 
-echo "Tagging new version in git"
-git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
+# echo "Tagging new version in git"
+# git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
 
-echo "Pushing latest commit to origin, with tags"
-git push origin master
-git push origin master --tags
+# echo "Pushing latest commit to origin, with tags"
+# git push origin master
+# git push origin master --tags
 
 echo 
 echo "Creating local copy of SVN repo ..."
@@ -53,7 +55,6 @@ svn co $SVNURL $SVNPATH
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
-# Add any large source files or other things to ignore here
 echo "Ignoring github specific files and deployment script"
 svn propset svn:ignore "deploy.sh
 README.md
@@ -66,11 +67,11 @@ cd $SVNPATH/trunk/
 svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
 svn commit --username=$SVNUSER -m "$COMMITMSG"
 
-echo "Creating new SVN tag & committing it"
-cd $SVNPATH
-svn copy trunk/ tags/$NEWVERSION1/
-cd $SVNPATH/tags/$NEWVERSION1
-svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
+# echo "Creating new SVN tag & committing it"
+# cd $SVNPATH
+# svn copy trunk/ tags/$NEWVERSION1/
+# cd $SVNPATH/tags/$NEWVERSION1
+# svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
 
 echo "Removing temporary directory $SVNPATH"
 rm -fr $SVNPATH/
